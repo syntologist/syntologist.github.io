0a32b6646a0c1dd99ad3e7d3b0f519a9bfb4e136
Giulia Mendes <giu.mendes@protonmail.com>
2023-09-07 19:56:04 -0300

    feat: base layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  theme: {
-    container: {
-      center: true,
-    },
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		container: {
+			center: true
+		},
+		extend: {
+			fontFamily: {
+				mono: ['JetBrains Mono', defaultTheme.fontFamily.mono]
+			}
+		}
+	},
+	plugins: [require('@tailwindcss/typography')]
+}
